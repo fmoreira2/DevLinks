f190531e3bdfc5e1f1a5163e23670fd766a32f41
Fernando Moreira <fernando.m.silva@outlook.com>
2023-03-01 11:28:29 -0300

    organização dos arquivos

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function toggleMode(){
     const html = document.documentElement;
 
+    //validação com if
     // if (html.classList.contains("light")) {
     //     html.classList.remove("light");
     // }
     // else{
     //     html.classList.add("light");
     // }
+
+    
     html.classList.toggle("light");
     
 
